4b164a448ab2d60ed2c4e62b6e2dd28988dd6ffd
plgaralde <pierr_garalde@yahoo.com>
2018-10-26 16:09:52 +0800

    Create plum.js

---
+++ js/plum.js
@@ -0,0 +1,31 @@
+'use strict';
+
+let doc = document,
+  plum = {}
+  ;
+  
+plum.json = {
+      company: 'ABC',
+      address: 'json/ABC.json',
+      action: {
+         items: ['checklist']
+      }
+   };
+    
+plum.doc = function () {
+   let tempDoc = createElement('div'),
+      company_name = createElement('div')
+   ;
+   
+   // header - company info
+   company_name.createTextNode = plum.json.company;
+   company_name.createTextNode = plum.json.address;
+   
+   // checklist
+   
+   tempDoc.appendChild(company_name);
+   
+   return tempDoc;
+};
+    
+doc.getElementsByTagName('body').innerHTML = plum.doc;
